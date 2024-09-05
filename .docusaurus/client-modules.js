8ad7af58830fbd33cb035b9f487223b1b21259a2
Alex Villa <115514619+aevilla160@users.noreply.github.com>
2024-09-05 11:12:51 -0700

    added new branch

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/avilla/Downloads/aevilla160.github.io/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/avilla/Downloads/aevilla160.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/avilla/Downloads/aevilla160.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/avilla/Downloads/aevilla160.github.io/src/css/custom.css"),
+];
